0aa277e002614c765a1d40363ebfa9604733057f
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2019-02-26 20:37:19 +0900

    일부 한글화

--- tg_bot/modules/sed.py
+++ tg_bot/modules/sed.py
@@ -71,7 +71,7 @@ def sed(bot: Bot, update: Update):
             check = re.match(repl, to_fix, flags=re.IGNORECASE)
 
             if check and check.group(0).lower() == to_fix.lower():
-                update.effective_message.reply_to_message.reply_text("Hey everyone, {} is trying to make "
+                update.effective_message.reply_to_message.reply_text("안녕하세요 여러분, {} is trying to make "
                                                                      "me say stuff I don't wanna "
                                                                      "say!".format(update.effective_user.first_name))
                 return
