a4d528beec7aa3dbf8bb50ec4977d347a69db1a6
KCPIT <kgu090716@gmail.com>
2019-02-26 16:43:13 +0900

    힌글화 완료

--- tg_bot/modules/backups.py
+++ tg_bot/modules/backups.py
@@ -22,8 +22,7 @@ def import_data(bot: Bot, update):
         try:
             file_info = bot.get_file(msg.reply_to_message.document.file_id)
         except BadRequest:
-            msg.reply_text("Try downloading and reuploading the file as yourself before importing - this one seems "
-                           "to be iffy!")
+            msg.reply_text("가져오기 전에 파일을 다운로드하여 자기것처럼 다시 업로드해 보세요 - 이 파일은 너무 많은 것 같아요!")
             return
 
         with BytesIO() as file:
@@ -33,8 +32,8 @@ def import_data(bot: Bot, update):
 
         # only import one group
         if len(data) > 1 and str(chat.id) not in data:
-            msg.reply_text("Theres more than one group here in this file, and none have the same chat id as this group "
-                           "- how do I choose what to import?")
+            msg.reply_text("이 파일에 그룹이 두 개 이상 있지만 이 그룹과 동일한 채팅 ID를 가진 그룹이 없어요. "
+                           "- 가져올 항목을 선택하려면 제가 어떻게 해야 할까요?")
             return
 
         # Select data source
@@ -47,16 +46,16 @@ def import_data(bot: Bot, update):
             for mod in DATA_IMPORT:
                 mod.__import_data__(str(chat.id), data)
         except Exception:
-            msg.reply_text("An exception occured while restoring your data. The process may not be complete. If "
-                           "you're having issues with this, message @MarieSupport with your backup file so the "
-                           "issue can be debugged. My owners would be happy to help, and every bug "
-                           "reported makes me better! Thanks! :)")
-            LOGGER.exception("Import for chatid %s with name %s failed.", str(chat.id), str(chat.title))
+            msg.reply_text("데이터를 복원하는 동안 오류가 발생하였어요. 절차가 완전하지 않을수도 있어요. 만약 "
+                           "이것에 문제가 있는 경우 문제 해결을 위해 @MarieSupport에 백업파일을 보내주세요. "
+                           "제 주인은 이것을 꼭 해결해줄 것이고 오류를 보고해주시면 제가 좀 더 나아질거예요! "
+                           "감사합니다 :)")
+            LOGGER.exception("아이디 : %s 이름 : %s 불러오기를 실패했어요.", str(chat.id), str(chat.title))
             return
 
         # TODO: some of that link logic
         # NOTE: consider default permissions stuff?
-        msg.reply_text("Backup fully imported. Welcome back! :D")
+        msg.reply_text("백업을 완전히 가져왔어요. 다시 돌아온걸 환영해요! :D)
 
 
 @run_async
@@ -69,10 +68,10 @@ def export_data(bot: Bot, update: Update):
 __mod_name__ = "Backups"
 
 __help__ = """
-*Admin only:*
- - /import: reply to a group butler backup file to import as much as possible, making the transfer super simple! Note \
-that files/photos can't be imported due to telegram restrictions.
- - /export: !!! This isn't a command yet, but should be coming soon!
+*관리자용 명령어*
+ - /import: 그룹 버틀러 백업 파일에게 답장 처리된 메시지를 다른 곳으로 불러와요. 변환을 매우 쉽게 할 수 있는 기능이지만, \
+ 텔레그램 규칙에 따라 사진과 파일은 이 기능으로 옮길 수 없어요.
+ - /export: !!! 아직 명령어는 아니지만 곧 출시될거예요.
 """
 IMPORT_HANDLER = CommandHandler("import", import_data)
 EXPORT_HANDLER = CommandHandler("export", export_data)
