b2390eef00e80a53b311d8470619d398f3d11b5d
KCPIT <kgu090716@gmail.com>
2019-03-01 12:51:38 +0900

    한글화 절반 완료

--- tg_bot/modules/misc.py
+++ tg_bot/modules/misc.py
@@ -18,55 +18,55 @@ from tg_bot.modules.helper_funcs.extraction import extract_user
 from tg_bot.modules.helper_funcs.filters import CustomFilters
 
 RUN_STRINGS = (
-    "Where do you think you're going?",
-    "Huh? what? did they get away?",
-    "ZZzzZZzz... Huh? what? oh, just them again, nevermind.",
-    "Get back here!",
-    "Not so fast...",
-    "Look out for the wall!",
-    "Don't leave me alone with them!!",
-    "You run, you die.",
-    "Jokes on you, I'm everywhere",
-    "You're gonna regret that...",
-    "You could also try /kickme, I hear that's fun.",
-    "Go bother someone else, no-one here cares.",
-    "You can run, but you can't hide.",
-    "Is that all you've got?",
-    "I'm behind you...",
-    "You've got company!",
-    "We can do this the easy way, or the hard way.",
-    "You just don't get it, do you?",
-    "Yeah, you better run!",
-    "Please, remind me how much I care?",
-    "I'd run faster if I were you.",
-    "That's definitely the droid we're looking for.",
-    "May the odds be ever in your favour.",
-    "Famous last words.",
-    "And they disappeared forever, never to be seen again.",
-    "\"Oh, look at me! I'm so cool, I can run from a bot!\" - this person",
-    "Yeah yeah, just tap /kickme already.",
-    "Here, take this ring and head to Mordor while you're at it.",
-    "Legend has it, they're still running...",
-    "Unlike Harry Potter, your parents can't protect you from me.",
-    "Fear leads to anger. Anger leads to hate. Hate leads to suffering. If you keep running in fear, you might "
-    "be the next Vader.",
-    "Multiple calculations later, I have decided my interest in your shenanigans is exactly 0.",
-    "Legend has it, they're still running.",
-    "Keep it up, not sure we want you here anyway.",
-    "You're a wiza- Oh. Wait. You're not Harry, keep moving.",
-    "NO RUNNING IN THE HALLWAYS!",
-    "Hasta la vista, baby.",
-    "Who let the dogs out?",
-    "It's funny, because no one cares.",
-    "Ah, what a waste. I liked that one.",
-    "Frankly, my dear, I don't give a damn.",
-    "My milkshake brings all the boys to yard... So run faster!",
-    "You can't HANDLE the truth!",
-    "A long time ago, in a galaxy far far away... Someone would've cared about that. Not anymore though.",
-    "Hey, look at them! They're running from the inevitable banhammer... Cute.",
-    "Han shot first. So will I.",
-    "What are you running after, a white rabbit?",
-    "As The Doctor would say... RUN!",
+    "어디로 갈 것 같아요?",
+    "네? 뭐라고요? 도망갔어요??",
+    "ZZzzZZzz... 네...? 뭐라고요? 아! 신경 쓰지 마세요.",
+    "다시 돌아오세요!",
+    "그렇게 빠르지 않아요...",
+    "벽을 조심하세요!",
+    "저를 내버려 두지 마세요!!",
+    "도망치면 죽어요",
+    "농담이예요, 전 어디에나 있어요",
+    "후회하게 될 거예요...",
+    "/kickme 도 해 보세요. 재미있다고 들었어요.",
+    "가서 다른 사람 괴롭혀요. 여기선 아무도 신경 안써요",
+    "도망칠 수는 있지만 숨을 수는 없어요.",
+    "그게 네가 가진 전부예요?",
+    "전 당신 뒤에 있어요...",
+    "단체가 있으시군요!",
+    "우리는 이것을 쉬운 방법으로, 또는 어려운 방법으로 할 수 있어요.",
+    "당신은 그냥 이해를 못하는군요, 맞죠?",
+    "네, 도망가는 게 좋을 거예요!",
+    "제발, 제가 얼마나 신경 쓰는지 알려주세요.",
+    "제가 당신이라면 더 빨리 달릴 텐데요.",
+    "우리가 찾는 안드로이드가 분명해요",
+    "승산이 있으시기를 빌어요.",
+    "유명한 유언.",
+    "그리고 그들은 영원히 사라졌고, 다시는 볼 수 없었습니다.",
+    "\"오, 절 봐요! 전 너무 멋져요, 전 봇으로부터 달릴 수 있어요\" - 사람",
+    "네, /kickme을 누르세요.",
+    "여기, 이 반지를 가져다가 모르도르로 가세요.",
+    "전설에 따르면, 그들은 여전히 달리고 있어요...",
+    "해리 포터와 달리, 당신의 부모님은 당신을 저로부터 보호할 수 없어요.",
+    "두려움은 분노로 이어집니다. 분노는 증오로 이어집니다. 증오는 고통으로 이어집니다. 겁에 질려 계속 도망가면 "
+    "계속 뛰면 제2의 베이더(Vader)가 될지도 몰라요.",
+    "여러 번 계산한 후에, 나는 당신의 속임수에 대한 나의 관심을 정확히 0으로 결정했어요.",
+    "전설에 따르면, 그들은 여전히 달리고 있어요.",
+    "계속 하세요, 당신이 여기 오길 바라는지 모르겠어요.",
+    "당신은 마법사인 거예요 - 잠깐만, 당신은 해리가 아니예요. 계속 움직이세요.",
+    "해프닝은 금지!",
+    "안녕, 자기야?",
+    "누가 개를 풀어줬나요?",
+    "아무도 신경 안써서 웃겨요.",
+    "아, 정말 아깝네요. 그거 마음에 들었어요.",
+    "솔직히 말씀해서, 전 개의치 않아요.",
+    "제 밀크쉐이크가 모든 아이들을 마당으로 데려왔어요! 그러니 더 빨리 달려요!",
+    "당신은 진실을 감당하지 못해요!",
+    "오래 전에, 멀리 있는 은하계에서... 누군가 신경 썼을 거예요. 하지만 이젠 아니예요.",
+    "저 사람들 좀 봐요! 그들은 피할 수 없는 망령에서 도망치고 있어요... 귀여워요.",
+    "한 형이 먼저 쐈어요. 저도 그럴까요?",
+    "흰 토끼야 뭘 쫓고 있는 거야?",
+    "의사가 말하듯이... 빨리 피해요.",
 )
 
 SLAP_TEMPLATES = (
@@ -194,7 +194,7 @@ def get_id(bot: Bot, update: Update, args: List[str]):
             user1 = update.effective_message.reply_to_message.from_user
             user2 = update.effective_message.reply_to_message.forward_from
             update.effective_message.reply_text(
-                "The original sender, {}, has an ID of `{}`.\nThe forwarder, {}, has an ID of `{}`.".format(
+                "원래 보낸사람, {}, 아이디 `{}`.\n전달자, {},  `{}`.".format(
                     escape_markdown(user2.first_name),
                     user2.id,
                     escape_markdown(user1.first_name),
@@ -202,16 +202,16 @@ def get_id(bot: Bot, update: Update, args: List[str]):
                 parse_mode=ParseMode.MARKDOWN)
         else:
             user = bot.get_chat(user_id)
-            update.effective_message.reply_text("{}'의 ID는 `{}` 입니다.".format(escape_markdown(user.first_name), user.id),
+            update.effective_message.reply_text("{}'의 ID는 `{}` 예요.".format(escape_markdown(user.first_name), user.id),
                                                 parse_mode=ParseMode.MARKDOWN)
     else:
         chat = update.effective_chat  # type: Optional[Chat]
         if chat.type == "private":
-            update.effective_message.reply_text("당신의 ID는 `{}` 입니다.".format(chat.id),
+            update.effective_message.reply_text("당신의 ID는 `{}` 예요.".format(chat.id),
                                                 parse_mode=ParseMode.MARKDOWN)
 
         else:
-            update.effective_message.reply_text("이 그룹의 ID는 `{}` 입니다.".format(chat.id),
+            update.effective_message.reply_text("이 그룹의 ID는 `{}` 예요.".format(chat.id),
                                                 parse_mode=ParseMode.MARKDOWN)
 
 
@@ -229,7 +229,7 @@ def info(bot: Bot, update: Update, args: List[str]):
     elif not msg.reply_to_message and (not args or (
             len(args) >= 1 and not args[0].startswith("@") and not args[0].isdigit() and not msg.parse_entities(
         [MessageEntity.TEXT_MENTION]))):
-        msg.reply_text("사용자의 ID를 추출할 수 없습니다.")
+        msg.reply_text("사용자의 ID를 추출할 수 없어요.")
         return
 
     else:
@@ -248,19 +248,19 @@ def info(bot: Bot, update: Update, args: List[str]):
     text += "\n영구 사용자 링크: {}".format(mention_html(user.id, "link"))
 
     if user.id == OWNER_ID:
-        text += "\n\n이 사람은 저의 주인입니다. - 전 절대 그들에게 나쁜 짓을 하지 않을 것입니다!"
+        text += "\n\n이 사람은 저의 주인이예요. - 전 그 상대로 아무것도 할 수가 없어요!"
     else:
         if user.id in SUDO_USERS:
-            text += "\nThis person is one of my sudo users! " \
-                    "Nearly as powerful as my owner - so watch it."
+            text += "\n이 사람은 저의 관리자들 중 한 명이예요! " \
+                    "그들은 제 주인만큼 강력해요."
         else:
             if user.id in SUPPORT_USERS:
-                text += "\nThis person is one of my support users! " \
-                        "Not quite a sudo user, but can still gban you off the map."
+                text += "\n이 사람은 제 후원자 중 한 명이에요! " \
+                        "관리자 사용자는 아니지만, 글로벌 밴을 할 수는 있어요."
 
             if user.id in WHITELIST_USERS:
-                text += "\nThis person has been whitelisted! " \
-                        "That means I'm not allowed to ban/kick them."
+                text += "\n이 사람은 화이트리스트에 속해있으신 분이예요! " \
+                        "ban/kick 등의 멍령어는 이분에게 사용하실 수 없어요."
 
     for mod in USER_INFO:
         mod_info = mod.__user_info__(user.id).strip()
@@ -325,13 +325,13 @@ def echo(bot: Bot, update: Update):
 
 @run_async
 def gdpr(bot: Bot, update: Update):
-    update.effective_message.reply_text("Deleting identifiable data...")
+    update.effective_message.reply_text("별 가능한 데이터 삭제...")
     for mod in GDPR:
         mod.__gdpr__(update.effective_user.id)
 
-    update.effective_message.reply_text("Your personal data has been deleted.\n\nNote that this will not unban "
-                                        "you from any chats, as that is telegram data, not Marie data. "
-                                        "Flooding, warns, and gbans are also preserved, as of "
+    update.effective_message.reply_text("개인 데이터가 삭제되었어요.\n\n참고,  Marie데이터가 아닌"
+                                        "Telegram데이터이기 때문에 어느 채팅방에서도 당신을 unban하지 않을 거예요."
+                                        "도배방지, 경고, 그리고 GBAN도 계속 남아있을 거예요. "
                                         "[this](https://ico.org.uk/for-organisations/guide-to-the-general-data-protection-regulation-gdpr/individual-rights/right-to-erasure/), "
                                         "which clearly states that the right to erasure does not apply "
                                         "\"for the performance of a task carried out in the public interest\", as is "
@@ -340,56 +340,56 @@ def gdpr(bot: Bot, update: Update):
 
 
 MARKDOWN_HELP = """
-Markdown is a very powerful formatting tool supported by telegram. {} has some enhancements, to make sure that \
-saved messages are correctly parsed, and to allow you to create buttons.
+Markdown은 telegram에서 지원되는 매우 강력한 도구예요. {} 저장된 메시지가 올바르게 구문 분석되었는지 확인하고 \
+단추를 만들 수 있도록 몇 가지 향상된 기능이 있어요.
 
-- <code>_italic_</code>: wrapping text with '_' will produce italic text
-- <code>*bold*</code>: wrapping text with '*' will produce bold text
-- <code>`code`</code>: wrapping text with '`' will produce monospaced text, also known as 'code'
-- <code>[sometext](someURL)</code>: this will create a link - the message will just show <code>sometext</code>, \
-and tapping on it will open the page at <code>someURL</code>.
-EG: <code>[test](example.com)</code>
+- <code>_italic_</code>: '_' 로 텍스트를 감싸면 기울어진 글자가 작성될 거예요. 
+- <code>*bold*</code>: '*' 로 텍스트를 감싸면 굵은 글자가 작성될 거예요. 
+- <code>`code`</code>: '`' 로 텍스트를 감싸면 'code'라고 하는 동일한 텍스트가 생성될 거예요.
+- <code>[보여질 메시지](URL)</code>: 링크를 만들어줘요. - 메시지는 <code>보여질 메시지</code> 라고 보여질 거지만, \
+그 메시지를 터치하면 <code>URL</code>로 들어가져요.
+예: <code>[test](example.com)</code>
 
-- <code>[buttontext](buttonurl:someURL)</code>: this is a special enhancement to allow users to have telegram \
-buttons in their markdown. <code>buttontext</code> will be what is displayed on the button, and <code>someurl</code> \
-will be the url which is opened.
-EG: <code>[This is a button](buttonurl:example.com)</code>
+- <code>[버튼에 보여질 메시지](URL)</code>: 이것은 telegram이 가지고 있는 특별한 \
+마크다운에서의 버튼 기능이예요. <code>버튼에 보여질 메시지</code> 가 버튼에 나타나고, 버튼을 누르면 <code>URL</code> \
+로 들어가져요.
+예: <code>[This is a button](buttonurl:example.com)</code>
 
-If you want multiple buttons on the same line, use :same, as such:
+같은 줄에 여러 개의 버튼을 사용하려면 :same을 사용하세요 :
 <code>[one](buttonurl://example.com)
 [two](buttonurl://google.com:same)</code>
-This will create two buttons on a single line, instead of one button per line.
+이렇게 하면 한 줄에 한 버튼이 아니라 한 줄에 두 개의 버튼이 생성되요.
 
-Keep in mind that your message <b>MUST</b> contain some text other than just a button!
+메시지에는 버튼이 아닌 텍스트가 포함되어야 해요!!!
 """.format(dispatcher.bot.first_name)
 
 
 @run_async
 def markdown_help(bot: Bot, update: Update):
     update.effective_message.reply_text(MARKDOWN_HELP, parse_mode=ParseMode.HTML)
-    update.effective_message.reply_text("Try forwarding the following message to me, and you'll see!")
-    update.effective_message.reply_text("/save test This is a markdown test. _italics_, *bold*, `code`, "
+    update.effective_message.reply_text("다음 메시지를 제게 전달해 보세요. 그러면 알게 될 거예요!")
+    update.effective_message.reply_text("/save 는 마크다운을 테스트 할 수 있어요. _기울어진 글자_, *굵은글자*, `code`, "
                                         "[URL](example.com) [button](buttonurl:github.com) "
                                         "[button2](buttonurl://google.com:same)")
 
 
 @run_async
 def stats(bot: Bot, update: Update):
-    update.effective_message.reply_text("Current stats:\n" + "\n".join([mod.__stats__() for mod in STATS]))
+    update.effective_message.reply_text("현재 상황:\n" + "\n".join([mod.__stats__() for mod in STATS]))
 
 
 # /ip is for private use
 __help__ = """
- - /id: get the current group id. If used by replying to a message, gets that user's id.
- - /runs: reply a random string from an array of replies.
- - /slap: slap a user, or get slapped if not a reply.
- - /info: get information about a user.
- - /gdpr: deletes your information from the bot's database. Private chats only.
+ - /id: 현재 그룹 ID를 가져와요. 만약 다른 사용자의 메시지를 답장하면 해당 사용자의 ID를 가져와요.
+ - /runs: 랜덤으로 아무 말을 해 드려요.
+ - /slap: 사용자에게 뺨을 때리거나, 응답이 없을 경우 뺨을 맞아요.
+ - /info: 사용자에 대한 정보를 가져와줘요.
+ - /gdpr: 봇 데이터베이스에서 정보를 삭제해요. (사적인 대화만)
 
- - /markdownhelp: quick summary of how markdown works in telegram - can only be called in private chats.
+ - /markdownhelp: 마크다운에 대해서 알려줘요 - 비공개 채팅에서만 알려줄 수 있어요.
 """
 
-__mod_name__ = "Misc"
+__mod_name__ = "기타"
 
 ID_HANDLER = DisableAbleCommandHandler("id", get_id, pass_args=True)
 IP_HANDLER = CommandHandler("ip", get_bot_ip, filters=Filters.chat(OWNER_ID))
