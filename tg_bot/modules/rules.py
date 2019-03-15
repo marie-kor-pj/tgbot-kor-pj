673a39def3bb610314c2409924e28042bdba7cc7
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2019-03-15 19:34:55 +0900

    한글화 완료

--- tg_bot/modules/rules.py
+++ tg_bot/modules/rules.py
@@ -39,16 +39,16 @@ def send_rules(update, chat_id, from_pm=False):
         bot.send_message(user.id, text, parse_mode=ParseMode.MARKDOWN)
     elif from_pm:
         bot.send_message(user.id, "그룹의 관리자가 이 채팅에 어떠한 규칙도 적용하지 않았어요. "
-                                  "이것은 이그룹에 규칙이 없다는것을 의미하지는 않을거예요!")
+                                  "이것은 이그룹에 규칙이 없다는것을 의미하지는 않을거에요!")
     elif rules:
-        update.effective_message.reply_text("이 그룹의 규칙을 확인하시려면 저에게 개인 메시지로 연락해요.",
+        update.effective_message.reply_text("이 그룹의 규칙을 확인하시려면 저에게 개인 메시지로 연락하세요.",
                                             reply_markup=InlineKeyboardMarkup(
                                                 [[InlineKeyboardButton(text="규칙",
                                                                        url="t.me/{}?start={}".format(bot.username,
                                                                                                      chat_id))]]))
     else:
         update.effective_message.reply_text("그룹의 관리자가 이 채팅에 어떠한 규칙도 적용하지 않았어요. "
-                                            "이것은 이그룹에 규칙이 없다는것을 의미하지는 않을거에요!")
+                                            "이것은 이 그룹에 규칙이 없다는것을 의미하지는 않을거에요!")
 
 
 @run_async
@@ -64,7 +64,7 @@ def set_rules(bot: Bot, update: Update):
         markdown_rules = markdown_parser(txt, entities=msg.parse_entities(), offset=offset)
 
         sql.set_rules(chat_id, markdown_rules)
-        update.effective_message.reply_text("이 그룹에 성공적으로 규칙을 적용함")
+        update.effective_message.reply_text("이 그룹에 성공적으로 규칙을 적용했어요")
 
 
 @run_async
@@ -72,7 +72,7 @@ def set_rules(bot: Bot, update: Update):
 def clear_rules(bot: Bot, update: Update):
     chat_id = update.effective_chat.id
     sql.set_rules(chat_id, "")
-    update.effective_message.reply_text("규칙을 성공적으로 지움")
+    update.effective_message.reply_text("규칙을 성공적으로 지어요요)
 
 
 def __stats__():
@@ -90,7 +90,7 @@ def __migrate__(old_chat_id, new_chat_id):
 
 
 def __chat_settings__(chat_id, user_id):
-    return "This chat has had it's rules set: `{}`".format(bool(sql.get_rules(chat_id)))
+    return "이 채팅의 규칙입니다: `{}`".format(bool(sql.get_rules(chat_id)))
 
 
 __help__ = """
