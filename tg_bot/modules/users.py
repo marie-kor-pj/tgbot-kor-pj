077cfe80e776648d7e4ddc805416aeaf5dd19498
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2020-02-10 15:02:08 +0900

    오타수정

--- tg_bot/modules/users.py
+++ tg_bot/modules/users.py
@@ -42,7 +42,7 @@ def get_user_id(username):
                 if excp.message == 'Chat not found':
                     pass
                 else:
-                    LOGGER.exception("사용자 ID를 추출하는 중 에러발생")
+                    LOGGER.exception("사용자 ID를 추출하는 중 에러 발생")
 
     return None
 
@@ -101,7 +101,7 @@ def chats(bot: Bot, update: Update):
 
 def __user_info__(user_id):
     if user_id == dispatcher.bot.id:
-        return """전 그들을 본 적이 있어요... 와. 그들이 저를 스토킹하고 있는거에요? 그들은 제가 있는 모든 장소에 있네요.... 오, 그건 저에요."""
+        return """전 그들을 본 적이 있어요... 와. 그들이 저를 스토킹 하고 있는 거에요? 그들은 제가 있는 모든 장소에 있네요.... 오, 그건 저예요."""
     num_chats = sql.get_user_num_chats(user_id)
     return """<code>{}</code> 개의 채팅방에서 봤어요.""".format(num_chats)
 
