845d11482f60c4104556bb41d9360a483ddb4de1
JaeBeom An <33373557+NewPremium@users.noreply.github.com>
2020-02-11 22:34:14 +0900

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -34,7 +34,7 @@ class Config(object):
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
     STRICT_GBAN = False
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'CAACAgUAAxkBAAED4zteQjLO2MumgWq17cyreRjorpVq7wACEQAD7vamIRlwRuKSNQPbGAQ'  # banhammer marie sticker
+    BAN_STICKER = 'CAACAgQAAxkBAAED4_FeQq0f3uGjymyvbnn9he5hJCJOTgAC0wIAAqN9MRWXv1dilHR9NhgE'  # banhammer marie sticker
     ALLOW_EXCL = False  # Allow ! commands as well as /
 
 
